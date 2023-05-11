a00f8093e1f9d17d1012ce10a5c3dbc00c88b90d
JP-Brito <jpbritoalm@gmail.com>
2023-05-11 14:44:25 -0300

    Primeiro commit

---
+++ src/App.tsx
@@ -0,0 +1,68 @@
+import { Post } from './components/Post';
+import { Header } from './components/Header';
+import { SideBar } from './components/SideBar';
+
+import styles from './App.module.css';
+import './global.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/Jp-Brito.png',
+      name: 'João Pedro',
+      role: 'Web desenvolvedor jr',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-05-10 13:35:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/JeffersonBrito.png',
+      name: 'Jefferson',
+      role: 'Web desenvolvedor pleno',
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      {
+        type: 'paragraph',
+        content:
+          'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',
+      },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2023-05-9 16:21:00'),
+  },
+];
+
+export function App() {
+  return (
+    <div>
+      <Header />
+      <div className={styles.wrapper}>
+        <SideBar />
+        <main>
+          {posts.map((post) => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            );
+          })}
+        </main>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/SideBar.tsx
@@ -0,0 +1,28 @@
+import { Avatar } from './Avatar';
+import styles from './Sidebar.module.css';
+import { PencilLine } from '@phosphor-icons/react';
+
+export function SideBar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1531581147762-5961e6e2e6b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
+        alt=""
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/Jp-Brito.png" />
+        <strong>João Pedro</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
